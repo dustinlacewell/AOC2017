0467d87bbf99acd80729d781e74b8c2ca2a23471
Dustin Lacewell <dlacewell@gmail.com>
2017-12-05 00:33:11 -0600

    Add Day 4 Part 1

---
+++ day4.fsx
@@ -0,0 +1,30 @@
+﻿open System
+open System.IO
+open System.Collections.Generic
+
+let tokenize (x:string) = List.ofSeq (x.Split(' '))
+
+let readLine (sr:StreamReader) = match sr.ReadLine() with
+    | null -> sr.Dispose(); None
+    | str -> Some(str, sr)
+
+let readLines (filename:string) =
+    new StreamReader(filename) |> Seq.unfold readLine
+
+let validate tokens =
+    tokens
+    |> Seq.countBy id
+    |> Seq.toList
+    |> Seq.filter (fun (x, n) -> n > 1)
+    |> Seq.length
+    |> (fun x -> x = 0)
+
+
+[<EntryPoint>]
+let main argv =
+    readLines "day4.txt"
+    |> Seq.map (tokenize >> validate)
+    |> Seq.filter (fun x -> x)
+    |> Seq.length
+    |> printfn "%A"
+    0
