18f8f823f12b3369314c49be09bc4968e09ba0c5
Dustin Lacewell <dlacewell@gmail.com>
2017-12-05 12:11:48 -0600

    Update 4B

---
+++ day4B.fsx
@@ -0,0 +1,22 @@
+﻿#load "utils.fsx"
+
+let lookup = [
+    ('a', 2); ('b', 3); ('c', 5); ('d', 7); ('e', 11); ('f', 13); ('g', 17); ('h', 19); ('i', 23);
+    ('j', 29); ('k', 31); ('l', 37); ('m', 41); ('n', 43); ('o', 47); ('p', 53); ('q', 59); ('r', 61);
+    ('s', 67); ('t', 71); ('u', 73); ('v', 79); ('w', 83); ('x', 89); ('y', 97); ('z', 101);] |> dict
+
+let computeHash (s:string) =
+    s |> Seq.fold (fun acc c -> acc * lookup.[c]) 1
+
+let isAnagram word1 word2 =
+    (computeHash word1) = (computeHash word2)
+
+[<EntryPoint>]
+let main argv =
+    Utils.readLines "day4.txt"
+    |> Seq.map (Utils.split ' ')
+    |> Seq.map (exhaust isAnagram)
+    |> Seq.filter (fun x -> not x)
+    |> Seq.length
+    |> printfn "%A"
+    0
--- utils.fsx
+++ utils.fsx
@@ -6,25 +6,25 @@ open System.IO
 open System.Collections.Generic
 
 
-let tokenize (d:char) (x:string) = List.ofSeq (x.Split(' '))
+let split (d:char) (x:string) = Seq.ofArray (x.Split(d))
 
 let readLine (sr:StreamReader) = match sr.ReadLine() with
-    | null -> sr.Dispose(); None
-    | str -> Some(str, sr)
+                                 | null -> sr.Dispose(); None
+                                 | str -> Some(str, sr)
 
 let readLines (filename:string) =
     new StreamReader(filename) |> Seq.unfold readLine
 
 // true if p for any two in hs
-let exhaust p (hs:_ list) =
+let exhaust p (hs:seq<_>) =
     let rec outer (o:int) =
         let rec inner (i:int) =
-            if i = hs.Length then
+            if i = Seq.length hs then
                 false
             else
-                p hs.[o] hs.[i] || inner (i+1)
+                p (Seq.item o hs) (Seq.item i hs) || inner (i+1)
 
-        if o = hs.Length - 1 then
+        if o = (Seq.length hs) - 1 then
             false
         else
             inner (o+1) || outer (o+1)
