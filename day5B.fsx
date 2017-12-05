2e766156b37f4693ec36ec057c1cf5628e5845d2
Dustin Lacewell <dlacewell@gmail.com>
2017-12-05 13:26:20 -0600

    Update Day 5 Part 2 to arrays

--- day5B.fsx
+++ day5B.fsx
@@ -1,19 +1,23 @@
 ﻿#load "utils.fsx"
 
-let rec solve sum pos (tape:Map<int,int>) =
-    match tape.TryFind pos with
-        | Some x ->
-            let offset = if x > 2 then -1 else 1
-            tape.Add (pos, (x + offset))
-            |> solve (sum + 1) (pos + x)
-        | None -> sum
+let rec solve sum pos (tape:int[]) =
+    if pos < 0 || pos > (tape.Length-1) then
+        sum
+    else
+        let offset = tape.[pos]
+        let change =
+            if offset > 2 then
+                -1
+            else
+                1
+        tape.[pos] <-  offset + change
+        solve (sum + 1) (pos + offset) tape
 
 [<EntryPoint>]
 let main argv =
     Utils.readLines "day5.txt"
     |> Seq.map int
-    |> Seq.mapi (fun i x -> (i, x))
-    |> Map.ofSeq
+    |> Array.ofSeq
     |> solve 0 0
     |> printfn "%A"
     0
